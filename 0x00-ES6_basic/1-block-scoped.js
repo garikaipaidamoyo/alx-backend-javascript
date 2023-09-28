88037f00e91376a4d4b51e82314b788091f4ec78
garikaipaidamoyo <garikaipaidamoyo@gmail.com>
2023-09-28 18:49:49 +0000

    Block Scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,6 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  var task = false;
+  var task2 = true;
+
+  if (trueOrFalse) {
+    // Remove the var declarations inside the conditional block
+    task = true;
+    task2 = false;
+  }
 
   return [task, task2];
 }
