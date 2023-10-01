ab411b7683faadf9cd5017fa8dc014d683e25503
garikaipaidamoyo <garikaipaidamoyo@gmail.com>
2023-10-01 10:42:54 +0000

    You used to attend a place like this at some point

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -2,4 +2,8 @@ export default class ClassRoom {
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
+
+  get maxStudentsSize() {
+    return this._maxStudentsSize;
+  }
 }
