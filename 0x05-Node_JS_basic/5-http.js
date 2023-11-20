82f81c53a0c74d6e1730c1f2421fae142b2c127d
garikaipaidamoyo <garikaipaidamoyo.gmail>
2023-11-20 12:35:10 +0000

    Create a more complex HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!\n');
+  } else if (req.url === '/students') {
+    countStudents('database.csv')
+      .then(() => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('This is the list of our students\n'); // Add the student list here
+      })
+      .catch((error) => {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end(error.message);
+      });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found\n');
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
