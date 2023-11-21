2cd6ba2c1ac99d6e0b664df5af9b70e7a715e002
garikaipaidamoyo <garikaipaidamoyo.gmail>
2023-11-21 17:18:47 +0000

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,21 @@
+// 1-stdin.js
+
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
+
   if (name) {
     console.log(`Your name is: ${name}`);
   }
+
+  // Move the closing statement outside the if block
   process.stdout.write('This important software is now closing\n');
   process.exit();
 });
 
+// Export the functionality
 module.exports = {
   process,
 };
