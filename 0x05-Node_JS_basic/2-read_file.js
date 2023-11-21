ef9b9d6021007673a3c4e1e5693868038a80ca9e
garikaipaidamoyo <garikaipaidamoyo.gmail>
2023-11-21 19:18:14 +0000

    Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,20 +3,25 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
-    const lines = data.split('\n').filter(Boolean);
+    const lines = data.split('\n').filter(line => line.trim() !== '');
     const csStudents = [];
     const sweStudents = [];
 
-    lines.forEach(line => {
-      const [firstName, , field] = line.split(',');
-      if (field === 'CS') {
-        csStudents.push(firstName);
-      } else if (field === 'SWE') {
-        sweStudents.push(firstName);
+    let totalStudents = 0;
+
+    lines.forEach((line, index) => {
+      if (index !== 0) {
+        const [firstName, , field] = line.split(',');
+        if (field === 'CS') {
+          csStudents.push(firstName);
+        } else if (field === 'SWE') {
+          sweStudents.push(firstName);
+        }
+        totalStudents += 1;
       }
     });
 
-    console.log(`Number of students: ${lines.length}`);
+    console.log(`Number of students: ${totalStudents}`);
     console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudents.join(', ')}`);
     console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudents.join(', ')}`);
   } catch (error) {
