14cd2693dcfd23297dc0e35d473e9622456cbfed
garikaipaidamoyo <garikaipaidamoyo@gmail.com>
2023-11-23 16:21:22 +0200

    changes

--- 0x00-python_variable_annotations/2-floor.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import math
-
-
-def floor(n: float) -> int:
-    """Returns the floor of a float as an integer."""
-    return math.floor(n)
--- 0x00-python_variable_annotations/3-to_str.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-
-def to_str(n: float) -> str:
-    """Converts a float to its string representation."""
-    return str(n)
